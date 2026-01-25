4b193e7cc1a97a1aa2ed8333da669de0f4c25c67
Vazpera <jthoesel10@hotmail.com>
2026-01-25 15:54:08 -0700

    Auto build system created

--- build.rs
+++ build.rs
@@ -2,4 +2,5 @@ fn main() {
     let proj_dir = env!("CARGO_MANIFEST_DIR");
     let db_url = format!("sqlite://{}/budge.db", proj_dir);
     println!("cargo:rustc-env=DATABASE_URL={}", db_url);
+    println!("cargo:rustc-env=SQLX_OFFLINE=true");
 }
